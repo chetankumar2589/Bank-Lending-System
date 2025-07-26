f2c275e88e7176568e54e46e5a5056b7c49daef4
chetankumarpatruni <chetankumarpatruni@gmail.com>
2025-07-26 14:10:26 +0530

    Frontend: Implemented React Router basename for consistent local/prod URLs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // For React 18
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+
+// Dynamically determine basename from process.env.PUBLIC_URL, which is set by React scripts
+// based on the "homepage" field in package.json during build.
+const basename = process.env.PUBLIC_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+    <BrowserRouter basename={basename}> {/* Add the basename prop here */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
